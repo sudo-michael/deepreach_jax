66feea7a9216fc305d54cfaad8a70782c02e7168
Michael Lu <lu.michael@outlook.com>
2023-04-01 02:21:57 -0700

    experiment with making it easier to export fun

--- experiment_scripts/hji_1E2P.py
+++ experiment_scripts/hji_1E2P.py
@@ -58,6 +58,124 @@ class DatasetState:
     mean: float = 0.25
     var: float = 0.5
 
+def create_normalization_fn(dataset_state):
+    alpha = jnp.array(
+        [
+            1,
+            dataset_state.alpha["x"],
+            dataset_state.alpha["y"],
+            dataset_state.alpha["x"],
+            dataset_state.alpha["y"],
+            dataset_state.alpha["x"],
+            dataset_state.alpha["y"],
+            dataset_state.alpha["theta"],
+            dataset_state.alpha["theta"],
+            dataset_state.alpha["theta"],
+        ]
+    )
+
+    beta = jnp.array(
+        [
+            0,
+            dataset_state.beta["x"],
+            dataset_state.beta["y"],
+            dataset_state.beta["x"],
+            dataset_state.beta["y"],
+            dataset_state.beta["x"],
+            dataset_state.beta["y"],
+            dataset_state.beta["theta"],
+            dataset_state.beta["theta"],
+            dataset_state.beta["theta"],
+        ]
+    )
+
+    def unnormalize_tcoords(normalized_tcoord):
+        # [-1, 1]^d to global frame
+        return normalized_tcoord * alpha + beta
+
+    def normalize_tcoords(unnormalized_tcoord):
+        # global frame to [-1, 1]^d
+        return (unnormalized_tcoord - beta) / alpha
+
+    def scale_costates(dVdx):
+        return dVdx / alpha[1:]
+    
+    return unnormalize_tcoords, normalize_tcoords, scale_costates
+
+def create_hj_fn(dataset_state):
+    unnormalize_tcoords, normalize_tcoords, scale_costates = create_normalization_fn(dataset_state)
+
+    @jax.vmap
+    def initial_value_function(normalized_tcoords):
+        # [state sequence: 0, 1,   2,   3     4,    5,    6,    7,       8,        9].
+        # [state sequence: t, x_e, y_e, x_p1, y_p1, x_p2, y_p2, theta_e, theta_p1, theta_p2].
+
+        tcoords = unnormalize_tcoords(normalized_tcoords)
+        lx1 = jnp.linalg.norm(tcoords[1:3] - tcoords[3:5]) - dataset_state.collision_r
+        lx2 = jnp.linalg.norm(tcoords[1:3] - tcoords[5:7]) - dataset_state.collision_r
+        lx = jnp.minimum(lx1, lx2)
+        lx = normalize_value_function(
+            lx, dataset_state.norm_to, dataset_state.mean, dataset_state.var
+        )
+        return lx
+
+    @jax.vmap
+    def compute_hamiltonian(nablaV, normalized_tcoords):
+        tcoords = unnormalize_tcoords(normalized_tcoords)
+        # [state sequence: 0, 1,   2,   3     4,    5,    6,    7,       8,        9].
+        # [state sequence: t, x_e, y_e, x_p1, y_p1, x_p2, y_p2, theta_e, theta_p1, theta_p2].
+
+        dVdx = nablaV[1:]
+
+        # TODO understand why we scale dVdx
+        dVdx = scale_costates(dVdx)
+
+        dVdx_e, dVdy_e, dVdtheta_e = dVdx[0], dVdx[1], dVdx[6]
+        dVdx_p1, dVdy_p1, dVdtheta_p1 = dVdx[2], dVdx[3], dVdx[7]
+        dVdx_p2, dVdy_p2, dVdtheta_p2 = dVdx[4], dVdx[5], dVdx[8]
+
+        x_e, y_e, theta_e = tcoords[1], tcoords[2], tcoords[7]
+        x_p1, y_p1, theta_p1 = tcoords[3], tcoords[4], tcoords[8]
+        x_p2, y_p2, theta_p2 = tcoords[5], tcoords[6], tcoords[9]
+
+        ham_evader = dataset_state.velocity_evader * dVdx_e * jnp.cos(
+            theta_e
+        ) + dataset_state.velocity_evader * dVdy_e * jnp.sin(theta_e)
+        ham_evader = ham_evader + dataset_state.omega_evader * jnp.abs(dVdtheta_e)
+
+        ham_persuer1 = dataset_state.velocity_persuer * dVdx_p1 * jnp.cos(
+            theta_p1
+        ) + dataset_state.velocity_persuer * dVdy_p1 * jnp.sin(theta_p1)
+        ham_persuer1 = ham_persuer1 - dataset_state.omega_persuer * jnp.abs(dVdtheta_p1)
+
+        ham_persuer2 = dataset_state.velocity_persuer * dVdx_p2 * jnp.cos(
+            theta_p2
+        ) + dataset_state.velocity_persuer * dVdy_p2 * jnp.sin(theta_p2)
+        ham_persuer2 = ham_persuer2 - dataset_state.omega_persuer * jnp.abs(dVdtheta_p2)
+
+        ham = ham_evader + ham_persuer1 + ham_persuer2
+
+        return ham
+
+    @jax.vmap
+    def compute_opt_ctrl_dstb_fn(nablaV):
+        # [state sequence: 0, 1,   2,   3     4,    5,    6,    7,       8,        9].
+        # [state sequence: t, x_e, y_e, x_p1, y_p1, x_p2, y_p2, theta_e, theta_p1, theta_p2].
+        dVdx = nablaV[1:]  
+        dVdtheta_e = dVdx[6]
+        dVdtheta_p1 = dVdx[7]
+        dVdtheta_p2 = dVdx[8]
+
+        # opt_ctrl to maximize V
+        opt_ctrl = dataset_state.omega_evader * (dVdtheta_e > 0) + -dataset_state.omega_evader * (dVdtheta_e <= 0)
+
+        # opt_dstb to maximize V
+        opt_dstb_p1 = -dataset_state.omega_persuer * (dVdtheta_p1 > 0) + dataset_state.omega_persuer * (dVdtheta_p1 <= 0)
+        opt_dstb_p2 = -dataset_state.omega_persuer * (dVdtheta_p2 > 0) + dataset_state.omega_persuer * (dVdtheta_p2 <= 0)
+
+        return (opt_ctrl,), (opt_dstb_p1, opt_dstb_p2)
+    return initial_value_function, compute_hamiltonian, compute_opt_ctrl_dstb_fn
+    
 
 def main(args):
     # normalization from world space to [-1, 1]^d
@@ -143,98 +261,8 @@ def main(args):
 
     ckpt = {"model": state, "config": vars(args), "dataset": dataset_state}
 
-    alpha = jnp.array(
-        [
-            1,
-            dataset_state.alpha["x"],
-            dataset_state.alpha["y"],
-            dataset_state.alpha["x"],
-            dataset_state.alpha["y"],
-            dataset_state.alpha["x"],
-            dataset_state.alpha["y"],
-            dataset_state.alpha["theta"],
-            dataset_state.alpha["theta"],
-            dataset_state.alpha["theta"],
-        ]
-    )
+    initial_value_function, compute_hamiltonian, compute_opt_ctrl_dstb_fn = create_hj_fn(dataset_state)
 
-    beta = jnp.array(
-        [
-            0,
-            dataset_state.beta["x"],
-            dataset_state.beta["y"],
-            dataset_state.beta["x"],
-            dataset_state.beta["y"],
-            dataset_state.beta["x"],
-            dataset_state.beta["y"],
-            dataset_state.beta["theta"],
-            dataset_state.beta["theta"],
-            dataset_state.beta["theta"],
-        ]
-    )
-
-    def unnormalize_tcoords(normalized_tcoord):
-        # [-1, 1]^d to global frame
-        return normalized_tcoord * alpha + beta
-
-    def normalize_tcoords(unnormalized_tcoord):
-        # global frame to [-1, 1]^d
-        return (unnormalized_tcoord - beta) / alpha
-
-    @jax.vmap
-    def initial_value_function(normalized_tcoords):
-        # [state sequence: 0, 1,   2,   3     4,    5,    6,    7,       8,        9].
-        # [state sequence: t, x_e, y_e, x_p1, y_p1, x_p2, y_p2, theta_e, theta_p1, theta_p2].
-
-        tcoords = unnormalize_tcoords(normalized_tcoords)
-        lx1 = jnp.linalg.norm(tcoords[1:3] - tcoords[3:5]) - dataset_state.collision_r
-        lx2 = jnp.linalg.norm(tcoords[1:3] - tcoords[5:7]) - dataset_state.collision_r
-        lx = jnp.minimum(lx1, lx2)
-        lx = normalize_value_function(
-            lx, dataset_state.norm_to, dataset_state.mean, dataset_state.var
-        )
-        return lx
-
-    def scale_costates(dVdx):
-        return dVdx / alpha[1:]
-
-    @jax.vmap
-    def compute_hamiltonian(nablaV, normalized_tcoords):
-        tcoords = unnormalize_tcoords(normalized_tcoords)
-        # [state sequence: 0, 1,   2,   3     4,    5,    6,    7,       8,        9].
-        # [state sequence: t, x_e, y_e, x_p1, y_p1, x_p2, y_p2, theta_e, theta_p1, theta_p2].
-
-        dVdx = nablaV[1:]  # (x y theta)
-
-        # TODO understand why we scale dVdx
-        dVdx = scale_costates(dVdx)
-
-        dVdx_e, dVdy_e, dVdtheta_e = dVdx[0], dVdx[1], dVdx[6]
-        dVdx_p1, dVdy_p1, dVdtheta_p1 = dVdx[2], dVdx[3], dVdx[7]
-        dVdx_p2, dVdy_p2, dVdtheta_p2 = dVdx[4], dVdx[5], dVdx[8]
-
-        x_e, y_e, theta_e = tcoords[1], tcoords[2], tcoords[7]
-        x_p1, y_p1, theta_p1 = tcoords[3], tcoords[4], tcoords[8]
-        x_p2, y_p2, theta_p2 = tcoords[5], tcoords[6], tcoords[9]
-
-        ham_evader = dataset_state.velocity_evader * dVdx_e * jnp.cos(
-            theta_e
-        ) + dataset_state.velocity_evader * dVdy_e * jnp.sin(theta_e)
-        ham_evader = ham_evader + dataset_state.omega_evader * jnp.abs(dVdtheta_e)
-
-        ham_persuer1 = dataset_state.velocity_persuer * dVdx_p1 * jnp.cos(
-            theta_p1
-        ) + dataset_state.velocity_persuer * dVdy_p1 * jnp.sin(theta_p1)
-        ham_persuer1 = ham_persuer1 - dataset_state.omega_persuer * jnp.abs(dVdtheta_p1)
-
-        ham_persuer2 = dataset_state.velocity_persuer * dVdx_p2 * jnp.cos(
-            theta_p2
-        ) + dataset_state.velocity_persuer * dVdy_p2 * jnp.sin(theta_p2)
-        ham_persuer2 = ham_persuer2 - dataset_state.omega_persuer * jnp.abs(dVdtheta_p2)
-
-        ham = ham_evader + ham_persuer1 + ham_persuer2
-
-        return ham
 
     dubins_3d_dataset_sampler = create_dataset_sampler(
         initial_value_function, args.num_states
@@ -244,6 +272,7 @@ def main(args):
     loss_fn = initialize_hji_loss(state, args.min_with, compute_hamiltonian)
 
     def val_fn(state, epoch):
+        unnormalize_tcoords, normalize_tcoords, scale_costates = create_normalization_fn(dataset_state)
         times = [0.0, 0.5 * (args.t_max - 0.1), (args.t_max - 0.1)]
         x_p1s = [-1, -1, -1, -1]
         y_p1s = [0, 0, 0, 0]
@@ -345,72 +374,70 @@ def main(args):
             args.experiment_name,
         )
 
+def get_args():
+    p = argparse.ArgumentParser()
+    # fmt: off
+    p.add_argument("--experiment-name", type=str, required=True)
+    p.add_argument("--wandb", action='store_true')
+    p.add_argument("--logging-root", type=str, default='logs')
+    p.add_argument('--epochs', type=int, default=100_000)
+    p.add_argument('--epochs-till-checkpoint', type=int, default=2_000)
+    p.add_argument('--pretrain-end', type=int, default=2_000)
+    p.add_argument('--counter-end', type=int, default=110_000)
+    p.add_argument('--batch_size', type=int, default=65_000)
+    p.add_argument('--samples-at-t-min', type=int, default=10_000)
+    p.add_argument('--lr', type=float, default=2e-5)
+    p.add_argument('--seed', type=int, default=1)
+    p.add_argument("--validate", action='store_true')
+    # siren
+    p.add_argument("--num-hl", type=int, default=2, required=False, 
+                    help="The number of hidden layers"
+    )
+    p.add_argument("--num-nl", type=int, default=512, required=False, 
+                    help="Number of neurons per hidden layer.",
+    )
+    p.add_argument("--periodic-transform", action='store_true',
+                    help="convert theta to cos(theta) sin(theta)",
+    )
+    # brt
+    p.add_argument("--min-with", type=str, default="target", required=False, choices=["none", "zero", "target"], 
+                    help="BRS vs BRT computation",
+    )
+    p.add_argument("--t-min", type=float, default=0.0, required=False, 
+                    help="Start time of the simulation",
+    )
+    p.add_argument("--t-max", type=float, default=2.0, required=False, 
+                    help="End time of the simulation"
+    )
+    # initial value function
+    p.add_argument("--collision-r", type=float, default=0.2, required=False, 
+                    help="Collision radius between vehicles",
+    )
+    # dynamics
+    p.add_argument("--num-states", type=int, default=10, required=False,  # 3 *(1e + 2p) + 1
+                    help="Number of states in system including time",
+    )
+    p.add_argument("--angle-alpha", type=float, default=1.2, required=False, 
+                    help="Angle alpha coefficient.",
+    )
+    p.add_argument("--velocity-e", type=float, default=0.22, required=False, 
+                    help="Velocity of Evader",
+    )
+    p.add_argument("--velocity-p", type=float, default=0.14, required=False, 
+                    help="Velocity of Persuer",
+    )
+    p.add_argument("--omega-e", type=float, default=2.84, required=False, 
+                    help="Turn Rate of Evader") 
+    p.add_argument("--omega-p", type=float, default=2.00, required=False, 
+                    help="Turn Rate of Persuer") 
+    # fmt: on
+    args = p.parse_args()
+
+    assert args.pretrain_end < args.epochs
+
+    return args
 
 if __name__ in "__main__":
-
-    def get_args():
-        p = argparse.ArgumentParser()
-        # fmt: off
-        p.add_argument("--experiment-name", type=str, required=True)
-        p.add_argument("--wandb", action='store_true')
-        p.add_argument("--logging-root", type=str, default='logs')
-        p.add_argument('--epochs', type=int, default=100_000)
-        p.add_argument('--epochs-till-checkpoint', type=int, default=2_000)
-        p.add_argument('--pretrain-end', type=int, default=2_000)
-        p.add_argument('--counter-end', type=int, default=110_000)
-        p.add_argument('--batch_size', type=int, default=65_000)
-        p.add_argument('--samples-at-t-min', type=int, default=10_000)
-        p.add_argument('--lr', type=float, default=2e-5)
-        p.add_argument('--seed', type=int, default=1)
-        p.add_argument("--validate", action='store_true')
-        # siren
-        p.add_argument("--num-hl", type=int, default=2, required=False, 
-                       help="The number of hidden layers"
-        )
-        p.add_argument("--num-nl", type=int, default=512, required=False, 
-                       help="Number of neurons per hidden layer.",
-        )
-        p.add_argument("--periodic-transform", action='store_true',
-                       help="convert theta to cos(theta) sin(theta)",
-        )
-        # brt
-        p.add_argument("--min-with", type=str, default="target", required=False, choices=["none", "zero", "target"], 
-                       help="BRS vs BRT computation",
-        )
-        p.add_argument("--t-min", type=float, default=0.0, required=False, 
-                       help="Start time of the simulation",
-        )
-        p.add_argument("--t-max", type=float, default=2.0, required=False, 
-                       help="End time of the simulation"
-        )
-        # initial value function
-        p.add_argument("--collision-r", type=float, default=0.2, required=False, 
-                       help="Collision radius between vehicles",
-        )
-        # dynamics
-        p.add_argument("--num-states", type=int, default=10, required=False,  # 3 *(1e + 2p) + 1
-                       help="Number of states in system including time",
-        )
-        p.add_argument("--angle-alpha", type=float, default=1.2, required=False, 
-                       help="Angle alpha coefficient.",
-        )
-        p.add_argument("--velocity-e", type=float, default=2.00, required=False, 
-                       help="Velocity of Evader",
-        )
-        p.add_argument("--velocity-p", type=float, default=0.14, required=False, 
-                       help="Velocity of Persuer",
-        )
-        p.add_argument("--omega-e", type=float, default=2.84, required=False, 
-                       help="Turn Rate of Evader") 
-        p.add_argument("--omega-p", type=float, default=2.00, required=False, 
-                       help="Turn Rate of Persuer") 
-        # fmt: on
-        args = p.parse_args()
-
-        assert args.pretrain_end < args.epochs
-
-        return args
-
     args = get_args()
 
     if args.wandb:
